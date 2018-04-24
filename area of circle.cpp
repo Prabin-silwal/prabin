2bf4d73f4caff2fdf19a8916f3f52afdb5f5660b
Prabin-silwal <38076127+Prabin-silwal@users.noreply.github.com>
2018-04-24 10:44:54 +0545

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float r,area;
+	printf("enter the radius:");
+	scanf("%f",&r);
+	area=((3.14)*r*r);
+	printf("the area of circle is %f",area);
+	getch();
+}
