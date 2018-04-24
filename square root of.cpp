db2b6a7b6b718a9c0183c84755773ac6c2e16c2f
Prabin-silwal <38076127+Prabin-silwal@users.noreply.github.com>
2018-04-24 10:46:25 +0545

    Add files via upload

---
+++ square root of.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+{
+	float a,b;
+	printf("Enter the number:");
+	scanf("%f",&a);
+	b=sqrt(a);
+	printf("the square root is %.2f",b);
+	getch();
+	
+}
